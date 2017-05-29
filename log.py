38437e18f24702f5296a812ae754ad38d88271d8
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-05-28 20:34:11 -0700

    Clean up code

--- log.py
+++ log.py
@@ -8,23 +8,6 @@ based on the data in the database.
 @Date: 2017-05-23
 """
 
-"""
-Satisfy the following 3 tasks:
-
-1. What are the most popular three articles of all time?
-Which articles have been accessed the most?
-Present this information as a sorted list with the most popular article
-at the top.
-
-2. Who are the most popular article authors of all time? That is, when you sum
-up all of the articles each author has written, which authors get the most page
-views? Present this as a sorted list with the most popular author at the top.
-
-3. On which days did more than 1 percent of requests lead to errors? The log
-table includes a column status that indicates the HTTP status code that the
-news site sent to the user's browser. (Refer back to this lesson if you want to
-review the idea of HTTP status codes.)
-"""
 import psycopg2
 
 DBNAME = "news"
@@ -137,7 +120,7 @@ def get_request_errors():
                 ORDER BY percent_of_errors DESC;
                 """
     c.execute(sql_query)
-    print("\nDays with more than 1% of requrest errors: \n")
+    print("\nDays with more than 1% of request errors: \n")
     rank = 1
     for article in c:
         print(str(rank) + '. ' + str(article[0]) + ' | ' + str(article[3])
